9618dd860074667296e4f4675d530c2c6b8a1e8e
HubiRto <hubert.rybicki.hr@gmail.com>
2024-04-20 21:18:55 +0200

    Finish

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using SecoundAnimalRestApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +18,123 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+Animal MapToAnimal(MySqlDataReader reader)
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    return new Animal
+    {
+        Id = reader.GetInt32(0),
+        Name = reader.GetString(1),
+        Description = reader.GetString(2),
+        Category = reader.GetString(3),
+        Area = reader.GetString(4),
+    };
+}
 
-app.MapGet("/weatherforecast", () =>
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+app.MapGet("/api/animals", async ([FromQuery] string orderBy = "Name") =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    var query = $"SELECT * FROM Animal ORDER BY {orderBy} ASC";
+    var animals = new List<Animal>();
+
+    await using (var connection = new MySqlConnection(connectionString))
+    {
+        var command = new MySqlCommand(query, connection);
+        await connection.OpenAsync();
+        await using (var reader = await command.ExecuteReaderAsync())
+        {
+            while (await reader.ReadAsync())
+            {
+                animals.Add(MapToAnimal(reader));
+            }
+        }
+    }
+
+    return Results.Ok(animals);
+});
+
+app.MapPost("/api/animals", async ([FromBody] Animal animal) =>
+{
+    const string query = "INSERT INTO Animal (Name, Description, Category, Area) VALUES (@Name, @Description, @Category, @Area)";
+
+    await using (var connection = new MySqlConnection(connectionString))
+    {
+        var command = new MySqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Name", animal.Name);
+        command.Parameters.AddWithValue("@Description", animal.Description);
+        command.Parameters.AddWithValue("@Category", animal.Category);
+        command.Parameters.AddWithValue("@Area", animal.Area);
+
+        await connection.OpenAsync();
+        await command.ExecuteNonQueryAsync();
+    }
+
+    return Results.StatusCode(201);
+});
+
+app.MapPut("/api/animals/{idAnimal}", async (int idAnimal, [FromBody] Animal animal) =>
+{
+    if (!await CheckAnimalExists(idAnimal))
+    {
+        return Results.NotFound($"No animal with ID {idAnimal} found.");
+    }
+    
+    const string query = "UPDATE Animal SET " +
+                         "Name = @Name, " +
+                         "Description = @Description, " +
+                         "Category = @Category, " +
+                         "Area = @Area " +
+                         "WHERE IdAnimal = @IdAnimal";
+
+    await using (var connection = new MySqlConnection(connectionString))
+    {
+        var command = new MySqlCommand(query, connection);
+        command.Parameters.AddWithValue("@IdAnimal", idAnimal);
+        command.Parameters.AddWithValue("@Name", animal.Name);
+        command.Parameters.AddWithValue("@Description", animal.Description);
+        command.Parameters.AddWithValue("@Category", animal.Category);
+        command.Parameters.AddWithValue("@Area", animal.Area);
+
+        await connection.OpenAsync();
+        await command.ExecuteNonQueryAsync();
+    }
+
+    return Results.Ok();
+});
+
+app.MapDelete("/api/animals/{idAnimal}", async (int idAnimal) =>
+{
+    if (!await CheckAnimalExists(idAnimal))
+    {
+        return Results.NotFound($"No animal with ID {idAnimal} found.");
+    }
+    
+    const string query = "DELETE FROM Animal WHERE IdAnimal = @IdAnimal";
+
+    await using (var connection = new MySqlConnection(connectionString))
+    {
+        var command = new MySqlCommand(query, connection);
+        command.Parameters.AddWithValue("@IdAnimal", idAnimal);
+
+        await connection.OpenAsync();
+        await command.ExecuteNonQueryAsync();
+    }
+
+    return Results.Ok();
+});
 
 app.Run();
+return;
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+async Task<bool> CheckAnimalExists(int idAnimal)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    const string query = "SELECT COUNT(1) FROM Animal WHERE IdAnimal = @IdAnimal";
+    await using var connection = new MySqlConnection(connectionString);
+    var cmd = new MySqlCommand(query, connection);
+    cmd.Parameters.AddWithValue("@IdAnimal", idAnimal);
+        
+    await connection.OpenAsync();
+    var result = Convert.ToInt32(await cmd.ExecuteScalarAsync());
+    return result > 0;
 }
+
---
+++ animal.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE Animal
+(
+    IdAnimal    int           NOT NULL,
+    Name        nvarchar(200) NOT NULL,
+    Description nvarchar(200) NULL,
+    Category    nvarchar(200) NOT NULL,
+    Area        nvarchar(200) NOT NULL,
+    CONSTRAINT Animal_pk PRIMARY KEY (IdAnimal)
+);
+
+INSERT INTO Animal (IdAnimal, Name, Description, Category, Area)
+VALUES (1, 'Lion', 'A large carnivorous feline mammal', 'Mammals', 'Africa'),
+       (2, 'Tiger', 'A large predatory feline mammal of Asia', 'Mammals', 'Asia'),
+       (3, 'Eagle', 'A bird of prey with a massive hooked bill and long broad wings', 'Birds', 'North America'),
+       (4, 'Shark', 'A long-bodied chiefly marine fish with a cartilaginous skeleton', 'Fish', 'Ocean'),
+       (5, 'Komodo Dragon', 'The largest living species of lizard', 'Reptiles', 'Indonesia'),
+       (6, 'Wombat', 'A short-legged, muscular quadrupedal marsupials', 'Marsupials', 'Australia'),
+       (7, 'Polar Bear', 'A bear native largely within the Arctic Circle', 'Mammals', 'Arctic'),
+       (8, 'Penguin', 'A flightless bird living almost exclusively in the southern hemisphere', 'Birds', 'Antarctica'),
+       (9, 'Anaconda', 'One of the largest and most powerful snakes in the world', 'Reptiles', 'South America'),
+       (10, 'Kangaroo', 'A large herbivore native to Australia, famous for its powerful hind legs', 'Marsupials',
+        'Australia');
